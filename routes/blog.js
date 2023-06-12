ebd9268852bdb2f89e1b25ae5a76be0910fa64b5
PFMello <philipmikh@gmail.com>
2023-06-12 13:16:03 -0300

    Crud finalizado !

--- routes/blog.js
+++ routes/blog.js
@@ -38,8 +38,60 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
-router.get("/post-detail", function (req, res) {
-  res.render("post-detail");
+router.get("/posts/:id", async function (req, res) {
+  const query = `
+  SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts 
+  INNER JOIN authors ON posts.author_id = authors.id
+  WHERE posts.id = ?
+  `;
+
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render("404");
+  }
+
+  const postData = {
+    ...posts[0],
+    date: posts[0].date.toISOString(),
+    humanReadableDate: posts[0].date.toLocaleDateString("pt-BR", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }),
+  };
+
+  res.render("post-detail", { post: postData });
+});
+
+router.get("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+
+  const query = `SELECT * FROM posts WHERE id = ?`;
+
+  const [posts] = await db.query(query, [postId]);
+
+  res.render("update-post", { post: posts[0] });
+});
+
+router.post("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+
+  const query = `UPDATE posts SET title = ?, summary = ?, body = ? WHERE id = ?`;
+
+  const data = [req.body.title, req.body.summary, req.body.content, postId];
+
+  await db.query(query, data);
+
+  res.redirect("/posts");
+});
+
+router.post("/posts/:id/delete", async function (req, res) {
+  const query = `DELETE FROM posts WHERE id = ?`;
+
+  await db.query(query, [req.params.id]);
+  res.redirect("/posts");
 });
 
 module.exports = router;
